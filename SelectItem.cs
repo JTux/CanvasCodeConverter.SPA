6c3308094c61e84aef0bc3c421829811f428de34
Joshua Tucker <the1jtux@gmail.com>
2021-11-01 15:55:17 -0400

    Updated to include Prism styling && Cleaned up setting toggling

---
+++ SelectItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CanvasCodeConverter
+{
+    public class SelectItem
+    {
+        public SelectItem() { }
+        public SelectItem(string label, string value)
+        {
+            Value = value;
+            Label = label;
+        }
+
+        public string Value { get; set; }
+        public string Label { get; set; }
+    }
+}
